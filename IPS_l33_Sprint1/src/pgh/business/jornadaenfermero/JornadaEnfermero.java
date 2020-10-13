e1cf97d6a0c6ba9410d19a39260e72ffa3e50ac4
Alberto <freicartobe@gmail.com>
2020-10-13 15:43:38 +0200

    Jornada Medico y Jornada Enfermero

--- IPS_l33_Sprint1/src/pgh/business/jornadaenfermero/JornadaEnfermero.java
+++ IPS_l33_Sprint1/src/pgh/business/jornadaenfermero/JornadaEnfermero.java
@@ -1,5 +1,7 @@
 package pgh.business.jornadaenfermero;
 
+import java.util.Date;
+
 public class JornadaEnfermero {
 	
 	private JornadaEnfermeroDTO j;
@@ -7,6 +9,42 @@ public class JornadaEnfermero {
 	public JornadaEnfermero(JornadaEnfermeroDTO jo) {
 		
 		this.j=jo;
+		
+	}
+	
+	public String getIdJornadaEnfermero() {
+		
+		return j.idJornadaEnfermero;
+		
+	}
+	
+	public String getIdEnfermero() {
+		
+		return j.idEnfermero;
+		
+	}
+	
+	public String getDias() {
+		
+		return j.dias;
+	
+	}
+	
+	public Date getDiaInicio() {
+		
+		return j.diaInicio;
+	
+	}
+
+	public Date getDiaFin() {
+	
+		return j.diaFin;
+	
+	}
+	
+	public String getIdHoraInicio() {
+		
+		return j.horaInicio;
 		
 	}
 
--- IPS_l33_Sprint1/src/pgh/business/jornadamedico/JornadaMedico.java
+++ IPS_l33_Sprint1/src/pgh/business/jornadamedico/JornadaMedico.java
@@ -1,5 +1,7 @@
 package pgh.business.jornadamedico;
 
+import java.util.Date;
+
 public class JornadaMedico {
 	
 	private JornadaMedicoDTO jm;
@@ -10,6 +12,42 @@ public class JornadaMedico {
 	
 	}
 	
+    public String getIdJornadaMedico() {
+		
+		return jm.idJornadaMedico;
+		
+	}
+	
+	public String getIdMedico() {
+		
+		return jm.idMedico;
+		
+	}
+	
+	public String getDias() {
+		
+		return jm.dias;
+	
+	}
+	
+	public Date getDiaInicio() {
+		
+		return jm.diaInicio;
+	
+	}
+
+	public Date getDiaFin() {
+	
+		return jm.diaFin;
+	
+	}
+	
+	public String getIdHoraInicio() {
+		
+		return jm.horaInicio;
+		
+	}
+
 	
 
 }
